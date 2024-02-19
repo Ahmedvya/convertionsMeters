c6b65328a6f4784b51858fd6af83a7cae1d2a1cf
Ahmedvya <159806398+Ahmedvya@users.noreply.github.com>
2024-02-19 11:50:33 -0500

    lab6

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a measurement in meters:");
+        if (input.hasNextDouble()) {
+            double meters = input.nextDouble();
+
+            double miles = meters / 1689.34;
+            double feet = meters / .3048;
+            double inches = meters / .0254;
+
+            System.out.println(" Measurement in miles:" + miles);
+            System.out.println("Measurement in feet:" + feet);
+            System.out.println("Measurement in inches: " + inches);
+
+        }else {
+            System.out.println("Invalid input for the measurement in meters.");
+        }
+        input.close();
+
+        }
+    }
